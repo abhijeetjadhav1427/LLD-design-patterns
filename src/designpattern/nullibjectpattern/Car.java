abc68f51e34af5ad4abd9bb5259259dcc92a28e8
AbhijeetJadhav <abhijeet.jadhav@cognizant.com>
2023-05-20 17:54:35 +0530

    null object design pattern demo

---
+++ src/designpattern/nullibjectpattern/Car.java
@@ -0,0 +1,17 @@
+package designpattern.nullibjectpattern;
+
+public class Car implements Vehicle {
+
+	@Override
+	public int getTankCapacity() {
+		// TODO Auto-generated method stub
+		return 10;
+	}
+
+	@Override
+	public int getSeatingCapacity() {
+		// TODO Auto-generated method stub
+		return 6;
+	}
+
+}
---
+++ src/designpattern/nullibjectpattern/NullObject.java
@@ -0,0 +1,17 @@
+package designpattern.nullibjectpattern;
+
+public class NullObject implements Vehicle {
+
+	@Override
+	public int getTankCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getSeatingCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
